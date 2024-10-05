980698c3c1e828004fdf791a4a93761d121e0da2
Your Name <you@example.com>
2024-10-04 20:04:00 -0400

    Task 2: Create Department class to manage employees and calculate salary

--- employee_management.js
+++ employee_management.js
@@ -14,6 +14,37 @@ Department: ${this.department}`
     };
 };
 //test use of getter
-const employee1 = new Employee ("Joe", 9000, "Manager", "Customer Service");
+const employee1 = new Employee ("Joe", 9000, "Representative", "Customer Service");
 console.log(`Task 1: Test Employee`);
 console.log(employee1.details);
+
+//Task 2: Create a Department Class
+class Department {
+    constructor(name,employees) {
+        this.name = name
+        this.employees = []};
+
+    addEmployee(employee){
+    this.employees.push(employee)
+    };
+
+    getTotalDepartmentSalary () {
+        return this.employees.reduce((total,employee)=>
+        total + employee.salary,0)
+    };
+    
+    get details () {
+        return `Department Name: ${this.name}`};
+};
+
+const department1 = new Department ("Customer Service")
+const employee2 = new Employee ("Kate", 4000, "Assistant", "Customer Service")
+
+department1.addEmployee(employee1);
+department1.addEmployee(employee2);
+let TotalCustomerServiceSalary = department1.getTotalDepartmentSalary();
+
+console.log(`Task 2: Create a Department Class`);
+console.log(department1.details);
+console.log(`Total Salary: $${TotalCustomerServiceSalary}`);
+
