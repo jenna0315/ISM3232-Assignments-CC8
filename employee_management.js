e591375f36f4390402b7d29fe2149789e8d40d13
Your Name <you@example.com>
2024-10-06 17:38:49 -0400

    Task 3: Create Manager class with inheritance from Employee

--- employee_management.js
+++ employee_management.js
@@ -8,7 +8,7 @@ class Employee {
     };
 get details () {
    return `Name: ${this.name}
-Salary: ${this.salary}
+Salary: $${this.salary}
 Position: ${this.position}
 Department: ${this.department}` 
     };
@@ -17,6 +17,7 @@ Department: ${this.department}`
 const employee1 = new Employee ("Joe", 9000, "Representative", "Customer Service");
 console.log(`Task 1: Test Employee`);
 console.log(employee1.details);
+
 
 //Task 2: Create a Department Class
 class Department {
@@ -32,9 +33,6 @@ class Department {
         return this.employees.reduce((total,employee)=>
         total + employee.salary,0)
     };
-    
-    get details () {
-        return `Department Name: ${this.name}`};
 };
 
 const department1 = new Department ("Customer Service")
@@ -44,7 +42,34 @@ department1.addEmployee(employee1);
 department1.addEmployee(employee2);
 let TotalCustomerServiceSalary = department1.getTotalDepartmentSalary();
 
+//test creation of department class
 console.log(`Task 2: Create a Department Class`);
-console.log(department1.details);
-console.log(`Total Salary: $${TotalCustomerServiceSalary}`);
+console.log(department1)
+console.log(`Total Customer Service Department Salary: $${TotalCustomerServiceSalary}`);
 
+
+//Task 3: Create a Manager Class that Inherits from Employee
+class Manager extends Employee {
+    constructor(name,salary,position,department,bonus){
+    super(name,salary,position,department)
+    this.bonus = bonus
+}
+get details () {
+    return `${super.details}
+Bonus: $${this.bonus}`
+     };
+ };
+ //create the customer service manager and add him to department 1
+ const CustomerServiceManager = new Manager ("Bob", 15000, "Manager", "Customer Service", 2000)
+ department1.addEmployee(CustomerServiceManager)
+
+
+//find total salary after adding the manager
+ TotalCustomerServiceSalary = department1.getTotalDepartmentSalary();
+
+ console.log(`Task 3: Create a Manager Class`);
+ console.log(`Test New Manager Details:`)
+ console.log(CustomerServiceManager.details)
+ console.log(`Test New Customer Service Department Salary:`)
+ console.log(`Total Salary: $${TotalCustomerServiceSalary}`);
+ 
